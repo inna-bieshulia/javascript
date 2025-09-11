c34706d9c0e4eafadbd8030e2238eaf7b637f402
Inna Bieshulia <inna.b@qamadness.com>
2025-09-11 23:34:07 +0300

    homework lesson 4

---
+++ lesson4/task1.js
@@ -0,0 +1,16 @@
+
+let number = 6;
+let number2 = 3;
+
+if (number % 2 === 0) {
+    console.log(`Number ${number} is even`);
+} else {
+    console.log(`Number ${number} is odd`);
+}
+
+
+if (number2 % 2 === 0) {
+    console.log(`Number ${number2} is even`);
+} else {
+    console.log(`Number ${number2} is odd`);
+}
